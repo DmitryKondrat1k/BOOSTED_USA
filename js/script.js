502cd101ac85d4d6847d46ff658f57189b706d78
Dima <dmitrykondratyuk4web@gmail.com>
2022-06-17 13:05:11 +0300

    firstOne

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+function ibg() {
+
+   let ibg = document.querySelectorAll(".ibg");
+   for (var i = 0; i < ibg.length; i++) {
+      if (ibg[i].querySelector('img')) {
+         ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+      }
+   }
+}
+
+ibg();
+
+let header__burger = document.querySelector('.header__burger');
+let header__menu = document.querySelector('.header__menu');
+let back = document.querySelector('body');
+let header__list = document.querySelector('.header__list');
+
+header__burger.onclick = function(){
+   header__burger.classList.toggle('active');
+   header__menu.classList.toggle('active');
+   back.classList.toggle('lock');
+};
+
+header__list.onclick = function () {
+   header__list.classList.remove('active');
+   back.classList.toggle('lock');
+}
+
+console.log(object);
